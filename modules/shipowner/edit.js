3a494627ed0ab93a2a29b1d104399bbb39b0b74e
kevinchen8621 <103777673@qq.com>
2014-12-29 18:09:00 +0800

    a

--- modules/shipowner/edit.js
+++ modules/shipowner/edit.js
@@ -1,1 +1,8 @@
-	
+define(function (require, exports, module) {
+	'use strict';
+	module.exports = function(app){
+		app.register.controller('ShipownerEditCtl', ['$scope', '$rootScope', '$routeParams', '$location', '$rest', function($scope, $rootScope, $routeParams, $location, $rest){
+
+		}]);
+	}
+});
--- modules/waybill/list.js
+++ modules/waybill/list.js
@@ -1,1 +1,8 @@
-	
+define(function (require, exports, module) {
+	'use strict';
+	module.exports = function(app){
+		app.register.controller('WaybillListCtl', ['$scope', '$rootScope', '$routeParams', '$location', '$rest', function($scope, $rootScope, $routeParams, $location, $rest){
+
+		}]);
+	}
+});
