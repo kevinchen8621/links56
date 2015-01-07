f492b46945f30d3be8d76ce90ef2edf5b2828456
kevinchen8621 <103777673@qq.com>
2015-01-07 16:34:29 +0800

    a

--- modules/order/track.js
+++ modules/order/track.js
@@ -11,25 +11,25 @@ define(function (require, exports, module) {
 			}else{
 				return $location.path("/shipowner_track");
 			}
-        var longitude = 121.506191;
-        var latitude = 31.245554;
-        $scope.mapOptions = {
-            center: {
-                longitude: longitude,
-                latitude: latitude
-            },
-            zoom: 17,
-            city: 'ShangHai',
-            markers: [{
-                longitude: longitude,
-                latitude: latitude,
-                icon: 'img/mappiont.png',
-                width: 49,
-                height: 60,
-                title: 'Where',
-                content: 'Put description here'
-            }]
-        };
+		        var longitude = 121.506191;
+		        var latitude = 31.245554;
+		        $scope.mapOptions = {
+		            center: {
+		                longitude: longitude,
+		                latitude: latitude
+		            },
+		            zoom: 17,
+		            city: 'ShangHai',
+		            markers: [{
+		                longitude: longitude,
+		                latitude: latitude,
+		                icon: 'assets/img/car.png',
+		                width: 49,
+		                height: 60,
+		                title: '王五 13042345607',
+		                content: '运单号123456'
+		            }]
+		        };
 
 			$scope.save = function(){
 				$scope.order._id = $scope.order._id || Date.now();
