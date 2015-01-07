84176646972f9dc8ea89941958c64ebfab59997b
kevinchen8621 <103777673@qq.com>
2015-01-07 17:38:50 +0800

    a

--- modules/order/list.js
+++ modules/order/list.js
@@ -1,8 +1,8 @@
 define(function (require, exports, module) {
 	'use strict';
 	module.exports = function(app){
-		app.register.controller('OrderListCtl', ['$scope', '$rootScope', '$routeParams', '$location', '$rest', function($scope, $rootScope, $routeParams, $location, $rest){
-
+		app.register.controller('OrderListCtl', ['$scope', '$rootScope', '$routeParams', '$location', '$window', function($scope, $rootScope, $routeParams, $location, $window){
+			$scope.orders = JSON.parse($window.localStorage.orders || "[]");
 		}]);
 	}
 });
