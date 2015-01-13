391c06f571b84e5cd5f31401dcee2601a6d986ee
kevinchen8621 <103777673@qq.com>
2015-01-13 11:41:26 +0800

    a

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ define(function(require, exports, module) {
 		app.config(['$routeProvider', function($routeProvider) {
 			$routeProvider
 				.when('/dashboard_demo', {controller: 'DashboardDemoCtl',controllerUrl: 'modules/dashboard/demo.js',templateUrl: 'modules/dashboard/demo.html'})
-				.when('/shipowner_authentication', {controller: 'ShipownerAuthenticationCtl',controllerUrl: 'modules/shipowner/authentication.js',templateUrl: 'modules/shipowner/authentication.html'})
+				.when('/dicshipowner_authentication', {controller: 'ShipownerAuthenticationCtl',controllerUrl: 'modules/shipowner/authentication.js',templateUrl: 'modules/shipowner/authentication.html'})
 				.when('/shipowner_dispatch', {controller: 'ShipownerDispatchCtl',controllerUrl: 'modules/shipowner/dispatch.js',templateUrl: 'modules/shipowner/dispatch.html'})
 				.when('/shipowner_plan', {controller: 'ShipownerPlanCtl',controllerUrl: 'modules/shipowner/plan.js',templateUrl: 'modules/shipowner/plan.html'})
 				.when('/shipowner_neworder', {controller: 'ShipownerNeworderCtl',controllerUrl: 'modules/shipowner/neworder.js',templateUrl: 'modules/shipowner/neworder.html'})
@@ -13,7 +13,7 @@ define(function(require, exports, module) {
 				.when('/shipowner_list', {controller: 'ShipownerListCtl',controllerUrl: 'modules/shipowner/list.js',templateUrl: 'modules/shipowner/list.html'})
 				.when('/shipowner_edit/:id', {controller: 'ShipownerEditCtl',controllerUrl: 'modules/shipowner/edit.js',templateUrl: 'modules/shipowner/edit.html'})
 				.when('/shipowner_edit', {controller: 'ShipownerEditCtl',controllerUrl: 'modules/shipowner/edit.js',templateUrl: 'modules/shipowner/edit.html'})
-				.when('/driver_authentication', {controller: 'DriverAuthenticationCtl',controllerUrl: 'modules/driver/authentication.js',templateUrl: 'modules/driver/authentication.html'})
+				.when('/dicdriver_authentication', {controller: 'DriverAuthenticationCtl',controllerUrl: 'modules/driver/authentication.js',templateUrl: 'modules/driver/authentication.html'})
 				.when('/driver_check', {controller: 'DriverCheckCtl',controllerUrl: 'modules/driver/check.js',templateUrl: 'modules/driver/check.html'})
 				.when('/driver_exception', {controller: 'DriverExceptionCtl',controllerUrl: 'modules/driver/exception.js',templateUrl: 'modules/driver/exception.html'})
 				.when('/driver_pay', {controller: 'DriverPayCtl',controllerUrl: 'modules/driver/pay.js',templateUrl: 'modules/driver/pay.html'})
@@ -33,6 +33,9 @@ define(function(require, exports, module) {
 				.when('/waybill_edit', {controller: 'WaybillEditCtl',controllerUrl: 'modules/waybill/edit.js',templateUrl: 'modules/waybill/edit.html'})
 				.when('/waybill_exception', {controller: 'WaybillExceptionCtl',controllerUrl: 'modules/waybill/exception.js',templateUrl: 'modules/waybill/exception.html'})
 				.when('/waybill_deal', {controller: 'WaybillDealCtl',controllerUrl: 'modules/waybill/deal.js',templateUrl: 'modules/waybill/deal.html'})
+				.when('/waybill_track', {controller: 'WaybillTrackCtl',controllerUrl: 'modules/waybill/track.js',templateUrl: 'modules/waybill/track.html'})
+				.when('/waybill_book', {controller: 'WaybillBookCtl',controllerUrl: 'modules/waybill/book.js',templateUrl: 'modules/waybill/book.html'})
+				.when('/waybill_pay', {controller: 'WaybillPayCtl',controllerUrl: 'modules/waybill/pay.js',templateUrl: 'modules/waybill/pay.html'})
 				.otherwise({
 					redirectTo: '/dashboard_demo'
 				});
---
+++ modules/waybill/pay.js
@@ -0,0 +1,48 @@
+define(function (require, exports, module) {
+	'use strict';
+	require('baidumap');
+	module.exports = function(app){
+		app.register.controller('WaybillPayCtl', ['$scope', '$rootScope', '$routeParams', '$location', '$window', function($scope, $rootScope, $routeParams, $location, $window){
+			$scope.orders = JSON.parse($window.localStorage.orders || "[]");
+			$scope.order = $scope.orders[0];
+		        var longitude = 121.506191;
+		        var latitude = 31.245554;
+		        $scope.mapOptions = {
+		            center: {
+		                longitude: longitude,
+		                latitude: latitude
+		            },
+		            zoom: 17,
+		            city: 'ShangHai',
+		            markers: [{
+		                longitude: longitude,
+		                latitude: latitude,
+		                icon: 'assets/img/car.png',
+		                width: 49,
+		                height: 60,
+		                title: '王五 13042345607',
+		                content: '运单号123456'
+		            }]
+		        };
+
+			$scope.save = function(){
+				$scope.order._id = $scope.order._id || Date.now();
+				$scope.orders = JSON.parse($window.localStorage.orders || "[]");
+				var idx = _.findIndex($scope.orders, function(o){return o._id == $scope.order._id;});
+				if(idx > -1){
+					$scope.orders.splice(idx,1,$scope.order);
+				}else{
+					$scope.orders.push($scope.order);	
+				}
+				$window.localStorage.orders =  JSON.stringify($scope.orders);
+			}
+
+			$scope.drop = function(){
+				$scope.order.status = "强行关闭";
+				$scope.save();
+				$location.path("/shipowner_track");
+			}
+
+		}]);
+	}
+});
